4d27eb24c7ef0d4637284fad064f1f1a18fb82db
Daniel Cardona <danielcar2na@gmail.com>
2022-07-29 00:33:16 -0500

    as

--- flutter_application_1/lib/home/View/home.dart
+++ flutter_application_1/lib/home/View/home.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home/view_model/home_view_model.dart';
+import 'package:flutter_application_1/login/model/seller.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
 
-  final viewModel = Get.put(HomeViewModel);
+  final viewModel = Get.find<HomeViewModel>();
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +16,12 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             child: Center(
                 child: Column(children: <Widget>[
-              card1(),
+              card1(viewModel.seller),
             ]))));
   }
 }
 
-Widget card1() {
+Widget card1(Seller seller) {
   return Container(
     width: Get.width * 0.9,
     height: Get.height * 0.2,
@@ -32,7 +33,7 @@ Widget card1() {
           child: Container(
             padding: const EdgeInsets.all(20),
             child: Column(
-              children: [Text('')],
+              children: [Text(seller.nombre)],
             ),
           )),
     ),
